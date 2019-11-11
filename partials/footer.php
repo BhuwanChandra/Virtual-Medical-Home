b5309aa2ba504a7dd1f157765125352d58a52160
BhuwanChandra <bhuwan7779@gmail.com>
2019-11-11 23:23:06 +0530

    started footer

---
+++ partials/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="newsletter">
+        SignUp for our weekly newsletter
+        <input type="email" name="email" placeholder="example@example.com">
+        <button type="submit">Subscribe</button>
+    </div>
+    <div class="footer_section">
+        <div class="sec1">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="firstaid.php">First Aid</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="">Doctors</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </div>
+        <div class="sec1">
+            <li><a href="index.php">Register as a Doctor</a></li>
+            <li><a href="firstaid.php">Login as a Doctor</a></li>
+            <li><a href="about.php">Privacy policy</a></li>
+            <li><a href="">Terms of use</a></li>
+        </div>
+    </div>
+    <div class="social_media">
+        
+    </div>
+    <div class="cpryt">
+
+    </div>
+</footer>
