83676a706cc601d8009ac69cbe0d045602ad1ba1
BhuwanChandra <bhuwan7779@gmail.com>
2019-11-19 04:23:13 +0530

    created doctor update page

--- updateUser.php
+++ updateUser.php
@@ -73,31 +73,4 @@ if($result){
     }
 }
 
-// $q = " select * from users where id = '$id' ";
-
-// $result = mysqli_query($con, $q);
-
-// $num = mysqli_num_rows($result);
-
-// if($num == 1){
-//     $result = mysqli_query($con, $q);
-//     while ($row = mysqli_fetch_assoc($result)) {
-//         $_SESSION['currentUser_id'] = $row['id'];
-//         $_SESSION['currentUser'] = $row['name'];
-//         $_SESSION['currentUserEmail'] = $row['email'];
-//         $_SESSION['currentUserDOB'] = $row['dob'];
-//         $_SESSION['currentUserAge'] = $row['age'];
-//         $_SESSION['currentUserHeight'] = $row['height'];
-//         $_SESSION['currentUserWeight'] = $row['weight'];
-//         $_SESSION['currentUserBloodGroup'] = $row['bloodGroup'];
-//         $_SESSION['currentUserBirthMark'] = $row['birthMark'];
-//         $_SESSION['currentUserFathersName'] = $row['fathersName'];
-//         $_SESSION['currentUserMothersName'] = $row['mothersName'];
-//         $_SESSION['user'] = "Patient";
-//         // header('location:User.php?user_id='.$id);
-//     }
-// }else {
-//     // header('location:login.php');
-// }
-
 ?>
